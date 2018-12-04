34d474d1a96de951417c89e25343924ca1126c59
Kerry Scharfglass <iborgel@gmail.com>
2018-12-03 19:57:35 -0800

    USB Keyboard test

--- mvp1/bolton_buttons_mvp1/bolton_buttons_mvp1.ino
+++ mvp1/bolton_buttons_mvp1/bolton_buttons_mvp1.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <usb_keyboard.h>
 
+void setup() {
+  Serial.begin(9600);
+
+  //TODO interrupt config
+
+  // wait for USB enumeration
+  delay(1000);
+  Keyboard.print("a");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //FIXME rm
+  Serial.println("loop");
+  delay(1000);
 
+  //check for key flags, and send data
 }
