637e2e84feef097759f785ecad86ed12e24cdb7e
Kerry Scharfglass <iborgel@gmail.com>
2018-12-05 21:46:26 -0800

    Display works!

--- mvp1/bolton_buttons_mvp1/bolton_buttons_mvp1.ino
+++ mvp1/bolton_buttons_mvp1/bolton_buttons_mvp1.ino
@@ -5,11 +5,7 @@
 #include <SPI.h>
 #include <Wire.h>
 
-// not sure if this is needed
-#define SSD1306_128_32
-
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <oled.h>
 
 #include <usb_keyboard.h>
 
@@ -18,10 +14,7 @@
 //TODO package
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-//#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(-1);
+OLED display = OLED(23, 22 ,NO_RESET_PIN,0x3C,SCREEN_WIDTH,SCREEN_HEIGHT, true);
 
 // TODO package
 static Adafruit_DotStar strip = Adafruit_DotStar(9, 12, 14, DOTSTAR_BRG);
@@ -52,19 +45,9 @@ void setup() {
   strip.show();
 
   //setup display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32?
+  display.begin();
+  display.print("asd");
   display.display();
-  display.clearDisplay();
-  display.drawPixel(10, 10, WHITE);
-  display.drawPixel(10, 11, WHITE);
-  display.drawPixel(10, 12, WHITE);
-  display.display();
-
-  /*
-  strip.setPixelColor(0, 10, 100, 50);
-  strip.setPixelColor(8, 200, 100, 50);
-  strip.show();
-  */
 
   //FIXME rm
   Serial.println("Done with setup");
