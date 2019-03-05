d4e154eac6cf2bc1582a8031f76cf12d12f6fa4e
Kerry Scharfglass <iborgel@gmail.com>
2019-03-05 10:11:42 -0800

    Starting MVP3 src from MVP1

---
+++ mvp3/bolton_buttons_mvp3/bolton_buttons_mvp3.ino
@@ -0,0 +1,240 @@
+
+
+#include <Adafruit_DotStar.h>
+#include <Bounce2.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include <oled.h>
+
+#include <usb_keyboard.h>
+#include <Encoder.h>
+#include "SoftPWM.h"
+
+#include "buttonmap.h"
+
+static int currentKeyConfig = 0;
+
+//TODO package
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+OLED display = OLED(23, 22 ,NO_RESET_PIN,0x3C,SCREEN_WIDTH,SCREEN_HEIGHT, true);
+
+// TODO package
+static Adafruit_DotStar strip = Adafruit_DotStar(9, 12, 14, DOTSTAR_BRG);
+static Bounce * buttonBouncers = new Bounce[NUM_BUTTONS];
+
+static Encoder knob(20, 21);
+
+static KeymapAssignment const * pressedKey;
+
+void setup() {
+  // wait for USB enumeration
+  delay(1000);
+  Serial.begin(9600);
+  Serial.println("Booted...");
+  
+  // attach bouncer to all buttons
+  ButtonAssignment *ba;
+  for(int i = 0; i < NUM_BUTTONS; i++) {
+    ba = &button_assignments[i];
+
+    buttonBouncers[i].attach(ba->pin, INPUT_PULLUP);
+    buttonBouncers[i].interval(40);
+    
+    Serial.printf("Attached button on pin %d\n", ba->pin);
+  }
+
+  strip.begin();
+  strip.show();
+
+  //setup display
+  display.begin();
+  display.print("Potato Is Nice");
+  display.display();
+  
+  switchKeyconfig();
+
+  // knob LEDs
+  SoftPWMBegin(SOFTPWM_INVERTED);
+  SoftPWMSet(17, 0);
+  SoftPWMSet(18, 0);
+  SoftPWMSet(19, 0);
+  SoftPWMSetFadeTime(ALL, 100, 100);
+  setKnobLEDWhite(0);
+
+  Serial.println("Done with setup");
+}
+
+static long lastKnob = 0;
+void loop() {
+  //check for key flags, and send data
+  // set the knob IO to low for just a moment to sense
+  
+  for(int i = 0; i < NUM_BUTTONS; i++) {
+    Bounce * b = &buttonBouncers[i];
+    b->update();
+    if(b->rose()) {
+      keyPressEvent(&button_assignments[i], false);
+    }
+    else if(b->fell()) {
+      keyPressEvent(&button_assignments[i], true);
+    }
+  }
+
+  // look for a knob change and dispatch events
+  if(knob.read() != lastKnob) {
+    long prevKnob = lastKnob;
+    lastKnob = knob.read();
+
+    // only dispatch an event on %4 (that matches the HW detents)
+    if(lastKnob % 4 == 0) {
+      // clockwise is more negative
+      knobEvent(lastKnob - prevKnob < 0);
+    }
+  }
+}
+
+void keyPressEvent(ButtonAssignment const * const b, bool const wasPress) {
+  // if this was a special key, fire the CB and return
+  if(b->special) {
+    return b->special(wasPress);
+  }
+
+  // normal key
+  KeymapAssignment const * const ka = getKeymappingForKey(b);
+  if(wasPress) {
+    //strip.setPixelColor(b->ledIndex, 255, 127, 0);
+    setKnobLED(rand() % 100, rand() % 100, rand() % 100);
+    
+    safeKeyboardPress(&ka->press);
+
+    // show rotate fxns on display
+    display.clear();
+    display.printf("%s\n", ka->name);
+    display.printf("Dec: %s\n", ka->decrement.name);
+    display.printf("Inc: %s\n", ka->increment.name);
+    display.display();
+
+    // mark this key as being pressed
+    pressedKey = ka;
+  }
+  else {
+    strip.setPixelColor(b->ledIndex, 0, 0, 0);
+    setKnobLEDWhite(0);
+    
+    safeKeyboardRelease(&ka->press);
+    
+    display.clear();
+    displayKeymapName();
+    display.display();
+
+    // unmark this key as being pressed
+    pressedKey = NULL;
+  }
+  strip.show();
+}
+
+void knobEvent(bool const wasIncrement) {
+  if(pressedKey) {
+    // release this key's pressed state
+    safeKeyboardRelease(&pressedKey->press);
+    
+    if(wasIncrement){
+      safeKeyboardPress(&pressedKey->increment);
+      //TODO need delay?
+      safeKeyboardRelease(&pressedKey->increment);
+    }
+    else {
+      safeKeyboardPress(&pressedKey->decrement);
+      //TODO need delay?
+      safeKeyboardRelease(&pressedKey->decrement);
+    }
+  }
+  else {
+    // layout-based default knob
+    KeymapAssignment const * const ka = &allKeymaps[currentKeyConfig].defaultKnob;
+    if(wasIncrement) {
+      safeKeyboardPress(&ka->increment);
+      safeKeyboardRelease(&ka->increment);
+    }
+    else {
+      safeKeyboardPress(&ka->decrement);
+      safeKeyboardRelease(&ka->decrement);
+    }
+  }
+}
+
+void modeButtonCB(bool const wasPress) {
+  if(wasPress) {
+    switchKeyconfig();
+  }
+}
+
+void encoderButtonCB(bool const wasPress) {
+  Serial.printf("Encoder button press was %d", wasPress);
+}
+
+const KeymapAssignment * const getKeymappingForKey(ButtonAssignment const * const butt) {
+  if(butt->assignmentMapIndex == -1) {
+    Serial.println("ERROR: Tried to get keymap for key without index");
+    while(1);
+  }
+  // the current group of key assignments
+  KeymapAssignment const * const ka = allKeymaps[currentKeyConfig].keymap;
+  // get the assignment for this key
+  return &ka[butt->assignmentMapIndex];
+}
+
+void switchKeyconfig() {
+  currentKeyConfig++;
+  if(allKeymaps[currentKeyConfig].name == NULL) {
+    currentKeyConfig = 0;
+  }
+
+  KeymapConfig const * const kc = &allKeymaps[currentKeyConfig];
+  Serial.printf("Selected keymap to %d (%s)\n", currentKeyConfig, kc->name);
+
+  // now do whatever to init this map
+  display.clear();
+  displayKeymapName();
+  //TODO display in a grid
+  /*
+  for(int i = 0; i < kc->keymapLen; i++) {
+    display.printf("%d:%s\n", i, kc->keymap[i].name);
+  }
+  */
+  display.display();
+}
+
+void displayKeymapName() {
+  KeymapConfig const * const kc = &allKeymaps[currentKeyConfig];
+  display.printf("%s\n", kc->name);
+}
+
+// perform all safety checks and press or release a key with modifiers
+void safeKeyboardPress(KeyShortcut const * const ks) {
+  if(ks->modifier != KS_NO_MODIFIER) {
+    Keyboard.press(ks->modifier);
+  }
+  if(ks->key != KS_INACTIVE) {
+    Keyboard.press(ks->key);
+  }
+}void safeKeyboardRelease(KeyShortcut const * const ks) {
+  if(ks->modifier != KS_NO_MODIFIER) {
+    Keyboard.release(ks->modifier);
+  }
+  if(ks->key != KS_INACTIVE) {
+    Keyboard.release(ks->key);
+  }
+}
+
+// set each param 0-100%
+void setKnobLED(uint8_t r, uint8_t g, uint8_t b) {
+  SoftPWMSetPercent(17, r);
+  SoftPWMSetPercent(18, g);
+  SoftPWMSetPercent(19, b);
+}
+void setKnobLEDWhite(uint8_t level) {
+  setKnobLED(level, level, level);
+}
---
+++ mvp3/bolton_buttons_mvp3/buttonmap.h
@@ -0,0 +1,192 @@
+#pragma once
+
+#include <stdint.h>
+
+// 9 keys (including esc)
+#define NUM_NORMAL_KEYS (9)
+// add rotary, bonus switch
+#define NUM_BUTTONS (NUM_NORMAL_KEYS+1+1)
+
+typedef void (*specialCallback)(bool const wasPress);
+
+// our one special CB is here, so we can refer to it in the assignment table. Fxn body is in the main ino
+void modeButtonCB(bool const wasPress);
+void encoderButtonCB(bool const wasPress);
+
+typedef struct {
+  int pin;
+  int assignmentMapIndex;     // this order matters! do not change it casually!
+  int ledIndex;
+  specialCallback special;
+} ButtonAssignment;
+
+// TODO separate pin from key
+ButtonAssignment button_assignments[NUM_BUTTONS] = {
+  //pin, keymap index, related LED chain offset
+  {2, 5, 7},   //5
+  {3, 7, 0},   //7
+  {4, 6, 6},   //6
+  {5, 2, 5},   //2
+  {6, 0, 0},   //esc
+  {7, 1, 8},   //1
+  {8, 4, 2},   //4
+  {9, 8, 1},   //8
+  {10, 3,3},   //3
+
+  // special keys should come after normal ones
+  {16, -1, 0, modeButtonCB},  // bonus button
+  {15, -1, 0, encoderButtonCB},  // encoder switch
+};
+
+#define KS_INACTIVE (0)
+#define KS_NO_MODIFIER (-1)
+
+typedef struct {
+  int32_t modifier;
+  int key;
+  const char *name;
+} KeyShortcut;
+
+typedef struct {
+  const char *name;
+  KeyShortcut press;
+  KeyShortcut const decrement;
+  KeyShortcut const increment;
+} KeymapAssignment;
+
+// keymaps for each button. ORDER MATTERS! When a key is pressed above
+// it will index into this array and invoke a key
+KeymapAssignment const keymapLayout[NUM_NORMAL_KEYS] = {
+  //esc
+  {"ESC",
+    .press={KS_NO_MODIFIER, KEY_ESC, "ESC"},
+    .decrement={MODIFIERKEY_CTRL, KEY_Z, "Undo"},
+    .increment={MODIFIERKEY_CTRL, KEY_Y, "Redo"},
+  },
+
+  //f1
+  {"Grid",
+    .press={KS_NO_MODIFIER,        KEY_HOME, "Home"},
+    .decrement={MODIFIERKEY_SHIFT, KEY_N, "Grid -"},
+    .increment={KS_NO_MODIFIER,    KEY_N, "Grid +"},
+  },
+
+  //f2
+  {"Layer Swap",
+    .press={KS_NO_MODIFIER, KEY_V, "Layer Swap"},
+    .decrement={},
+    .increment={},
+  },
+  //f3
+  {"Trace",
+    .press={KS_NO_MODIFIER,        KEY_X, "Start Track"},    // start new trace
+    .decrement={MODIFIERKEY_CTRL,  KEY_W, "Track Width -"},
+    .increment={KS_NO_MODIFIER,    KEY_W, "Track Width +"}, 
+  },
+  //f4
+  {"Move",
+    .press={KS_NO_MODIFIER,        KEY_M, "Move"},
+    .decrement={KS_NO_MODIFIER,    KEY_R, "Rotate+"},
+    .increment={MODIFIERKEY_SHIFT, KEY_R, "Rotate-"},
+  },
+  //f5
+  {"Recenter",
+    .press={MODIFIERKEY_CTRL,      KEY_F5, "Region Zoom"},
+    .decrement={KS_NO_MODIFIER,    KEY_F2, "Zoom Out"},
+    .increment={KS_NO_MODIFIER,    KEY_F1, "Zoom In"},
+  },
+  //f6
+  {"Delete",
+    .press={KS_NO_MODIFIER, KEY_DELETE, "Delete"},
+    .decrement={},
+    .increment={},
+  },
+  //f7
+  {"Delete",
+    .press={KS_NO_MODIFIER,    KEY_F, "Flip"},
+    .decrement={},
+    .increment={},
+  },
+  //f8
+  {"Edit",
+    .press={KS_NO_MODIFIER,        KEY_E, "Edit"},
+    .decrement={},
+    .increment={},
+  },
+};
+KeymapAssignment const keymapTest[NUM_NORMAL_KEYS] = {
+  {"ESC",
+    .press={KS_NO_MODIFIER, KEY_E, "ESC"},
+    .decrement={},
+    .increment={},
+  },
+
+  //f1
+  {"f1",
+    .press={KS_NO_MODIFIER, KEY_1, "1"},
+    .decrement={KS_NO_MODIFIER, KEY_1, "1-"},
+    .increment={KS_NO_MODIFIER, KEY_1, "1+"},
+  },
+  //f2
+  {"f2",
+    .press={KS_NO_MODIFIER, KEY_2, "2"},
+    .decrement={},
+    .increment={},
+  },
+  {"f3",
+    .press={KS_NO_MODIFIER, KEY_3, "3"},
+    .decrement={},
+    .increment={},
+  },
+  {"f4",
+    .press={KS_NO_MODIFIER, KEY_4, "4"},
+    .decrement={},
+    .increment={},
+  },
+  {"f5",
+    .press={KS_NO_MODIFIER, KEY_5, "5"},
+    .decrement={},
+    .increment={},
+  },
+  {"f6",
+    .press={KS_NO_MODIFIER, KEY_6, "6"},
+    .decrement={},
+    .increment={},
+  },
+  {"f7",
+    .press={KS_NO_MODIFIER, KEY_7, "7"},
+    .decrement={},
+    .increment={},
+  },
+  {"f8",
+    .press={KS_NO_MODIFIER, KEY_8, "8"},
+    .decrement={},
+    .increment={},
+  },
+};
+
+typedef struct {
+  const char *name;
+  int keymapLen;
+  KeymapAssignment const * const keymap;
+  KeymapAssignment defaultKnob;
+} KeymapConfig;
+
+KeymapAssignment defaultDialLayout = {
+  "Dial Zoom", .press = {},
+  .decrement={KS_NO_MODIFIER,    KEY_F2, "Zoom Out"},
+  .increment={KS_NO_MODIFIER,    KEY_F1, "Zoom In"},
+};
+KeymapAssignment defaultDialIdle = {
+  "Dial Zoom", .press = {},
+  .decrement={KS_NO_MODIFIER,    KEY_UP, "Scroll Up"},
+  .increment={KS_NO_MODIFIER,    KEY_DOWN, "Scroll Down"},
+};
+
+// the master structure of maps
+KeymapConfig const allKeymaps[] = {
+  {"KiCAD: Schematic", NUM_NORMAL_KEYS, keymapTest, defaultDialLayout},
+  {"KiCAD: Layout", NUM_NORMAL_KEYS, keymapLayout, defaultDialLayout},
+  {"Idle", 0, keymapLayout, defaultDialIdle},
+  {NULL},   // end flag
+};
