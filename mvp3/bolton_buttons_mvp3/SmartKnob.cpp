892c1a9e6cbc49ed91b2c56b5f1e09ece2a01042
Kerry Scharfglass <iborgel@gmail.com>
2019-03-07 08:42:14 -0800

    Add a divisor to knob changes/counts

--- mvp3/bolton_buttons_mvp3/SmartKnob.cpp
+++ mvp3/bolton_buttons_mvp3/SmartKnob.cpp
@@ -3,18 +3,23 @@
 #include <Encoder.h>
 
 bool SmartKnob::didChange() {
-  if(this->knob.read() != this->lastPosition) {
-    return true;
+  long d = this->lastPosition - this->knob.read();
+  // only count it as changed through the lens of the divisor
+  if(d) {
+    // if the change is in excess of one divisor
+    if(abs(d) >= this->divisor){
+      return true;
+    }
   }
   return false;
 }
 
 long SmartKnob::getChange() {
-  // TODO return how much changed from last time THIS was called
+  // return how much changed from last time THIS FXN was called
   long prevKnob = this->lastPosition;
   this->lastPosition = this->knob.read();
   
-  return this->lastPosition - prevKnob;
+  return (this->lastPosition - prevKnob) / this->divisor;
 }
 
 long SmartKnob::getPosition() {
--- mvp3/bolton_buttons_mvp3/SmartKnob.h
+++ mvp3/bolton_buttons_mvp3/SmartKnob.h
@@ -6,7 +6,7 @@
 class SmartKnob 
 {
 public:
-  SmartKnob(uint8_t pinA, uint8_t pinB): knob(Encoder(pinA, pinB)), lastPosition(0) {}
+  SmartKnob(uint8_t pinA, uint8_t pinB, uint8_t divisor = 4): knob(Encoder(pinA, pinB)), divisor(divisor), lastPosition(0) {}
 
   bool didChange();
   long getChange();
@@ -16,5 +16,6 @@ private:
   // gets re-init later on construction of SmartKnob
   Encoder knob;
 
+  uint8_t divisor;
   long lastPosition;
 };
